52e1e52d4821186fcbc0efc13f64c53228eeb338
tester-5 <61382661+tester-5@users.noreply.github.com>
2021-05-09 02:55:49 +0200

    Update Chekerfb.py

--- Chekerfb.py
+++ Chekerfb.py
@@ -5,17 +5,10 @@ import requests
 
 ani="""
 
-- FOR INFORMATION ✅
-- Welcome To HAMA CODER ⚡️💻
-
-- Shared Free Tools And Paid Tools 
-- To Make Special Program
-- Owner :
-- @COD3ER
-- @hama_software 
+- INFORMATION ✅
+- Owner : @Hideable
 - Languages : VB NET ,  C# , PYTHON
 
-My Instagram : danyar.software
 
 
 -----------------
